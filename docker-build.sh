94ab772c9ef741a5b31b2c8cbcfe63b22ad47f7c
Michael Klishevich <busation@gmail.com>
2018-05-27 22:33:53 +0300

    push image to docker hub

--- docker-build.sh
+++ docker-build.sh
@@ -10,12 +10,12 @@ fi
 
 echo "Docker image version: $appVersion"
 
-image="$appName:$appVersion"
+image="klishevich/$appName:$appVersion"
 
 tar -cf build.tar .build node_modules
 
 docker build -f ./Dockerfile -t $image .
 
-#docker push $image
+docker push $image
 
 rm build.tar
