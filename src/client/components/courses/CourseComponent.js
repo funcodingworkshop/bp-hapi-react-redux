74feed7fe1f7abebcccd6fc96f04beba1dd0e0eb
Danny Caesar <yadenis@list.ru>
2018-07-26 18:14:11 +0300

    Minor changes

--- src/client/components/courses/CourseComponent.js
+++ src/client/components/courses/CourseComponent.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import '../../css/global.css';
+
 class CourseComponent extends Component {
 	render(){
 		return (
 			<div>
+				<div className="upper-test"> blblb
+					<div className="test">Test</div>
+				</div>
 				<div>course id</div>
 				<div>course name</div>
 				<div>course unique code</div>
