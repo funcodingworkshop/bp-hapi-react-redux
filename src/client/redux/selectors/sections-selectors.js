52cc48526755e139c068a3d7803959480bbc8bcb
alfixed <alfixed@gmail.com>
2018-10-08 17:38:32 +0300

    Add redux sections selectors

---
+++ src/client/redux/selectors/sections-selectors.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+export function selectSections(state) {
+  return state.sections.list;
+}
+
+export function selectCurrentSection(state) {
+  return state.sections.current;
+}
