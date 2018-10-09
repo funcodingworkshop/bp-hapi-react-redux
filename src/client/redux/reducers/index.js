80a8317c6853f36af0d3fdbd844b81fed45bb70b
alfixed <alfixed@gmail.com>
2018-10-09 18:17:32 +0300

    Edit combine reducers

--- src/client/redux/reducers/index.js
+++ src/client/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import appReducer from './app-reducer';
 import coursesReducer from './courses-reducer';
+import sectionsReducer from './sections-reducer';
 
 export default combineReducers({
   router: routerReducer,
   app: appReducer,
-  courses: coursesReducer
+  courses: coursesReducer,
+  sections: sectionsReducer
 });
