3940b26bb51ce6c6dfd0759ab9583da03169cd16
alfixed <alfixed@gmail.com>
2018-10-15 18:31:48 +0300

    Add watch sections

--- src/client/redux/sagas/index.js
+++ src/client/redux/sagas/index.js
@@ -5,6 +5,11 @@ import { watchFetchCourse } from './courses-fetch-course-saga';
 import { watchAddCourse } from './courses-add-course-saga';
 import { watchDeleteCourse } from './courses-delete-course-saga';
 import { watchUpdateCourse } from './courses-update-course-saga';
+import { watchFetchSections } from './sections-fetch-sections-saga';
+import { watchFetchSection } from './sections-fetch-section-saga';
+import { watchAddSection } from './sections-add-section-saga';
+import { watchDeleteSection } from './sections-delete-section-saga';
+import { watchUpdateSection } from './sections-update-section-saga';
 import { watchSignUp } from './app-sign-up-saga';
 import { watchSignIn } from './app-sign-in-saga';
 import { watchFetchAccount } from './app-fetch-account-saga';
@@ -18,6 +23,11 @@ export default function* rootSaga() {
     watchAddCourse(),
     watchDeleteCourse(),
     watchUpdateCourse(),
+    watchFetchSections(),
+    watchFetchSection(),
+    watchAddSection(),
+    watchDeleteSection(),
+    watchUpdateSection(),
     watchSignUp(),
     watchSignIn(),
     watchFetchAccount(),
